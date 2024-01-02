edc49a9b575ae5e5124dba32da0f4ef3c1a99884
ABU SAID <abusaid7388@gmail.com>
2024-01-02 16:06:56 +0600

    Update pm2_runner.sh

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-  sudo npm install -g pm2 && pm2 start server.js --name=BackendAPI
+  sudo npm install -g pm2 && sudo pm2 start server.js --name=BackendAPI
 else
-  pm2 restart BackendAPI
+  sudo pm2 restart BackendAPI
 fi
